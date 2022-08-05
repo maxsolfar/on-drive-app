d1ebe03eb4006ab288017a610a7f75e71880ecdc
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-08-04 22:22:35 -0500

    Fix-Bugs-Home Style

--- frontend/src/components/containers/NewNavBar/NewNavBar.jsx
+++ frontend/src/components/containers/NewNavBar/NewNavBar.jsx
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 
 import iconHome from "../../../assets/NavBar/icon-home.svg";
 import iconSearch from "../../../assets/NavBar/icon-search.svg";
+import iconCart from "../../../assets/NavBar/icon-cart.svg";
 import iconSettings from "../../../assets/NavBar/icon-user.svg";
 import shortLogo from "../../../assets/Logo/on.png";
 import notifications from "../../../assets/NavBar/icon-bell.png";
@@ -17,9 +18,14 @@ const NewNavBar = () => {
     <nav className={styles.ContainerNavBar}>
       <img src={shortLogo} className={styles.Logo} alt="logo-on-drive" />
       <ul className={styles.NavBar}>
-        <li className={styles.NavItem}><Link to={"/"}><img src={iconHome} alt="icon-navbar-img" /></Link></li>
-        <li className={styles.NavItem}><Link to={"/search"}><img src={iconSearch} alt="icon-navbar-img" /></Link></li>
-        <li className={styles.NavItem}><Link to={"/profile"}><img src={iconSettings} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.DesktopMenu}`}><Link to={"/"}><img src={iconHome} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.DesktopMenu}`}><Link to={"/search"}><img src={iconSearch} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.DesktopMenu}`}><Link to={"/cart"}><img src={iconCart} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.DesktopMenu}`}><Link to={"/profile"}><img src={iconSettings} alt="icon-navbar-img" /></Link></li>
+         
+        <li className={`${styles.NavItem} ${styles.MobileMenu}`}><Link to={"/"}><img src={iconHome} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.MobileMenu}`}><Link to={"/search"}><img src={iconSearch} alt="icon-navbar-img" /></Link></li>
+        <li className={`${styles.NavItem} ${styles.MobileMenu}`}><Link to={"/profile"}><img src={iconSettings} alt="icon-navbar-img" /></Link></li>
       </ul>
       <section className={styles.ProfileIcons}>
         <Link to={"/"}><img className={styles.Notifications} src={notifications} alt="icon-notification-img" /><span className={styles.NotificationsNumber}>3</span></Link>
